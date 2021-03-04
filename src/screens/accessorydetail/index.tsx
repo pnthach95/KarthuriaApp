c8ba5840410307d1d3e7d4f384625144a25edbd4
Pham Ngoc Thach <thachxyz123@gmail.com>
2021-03-04 22:03:55 +0700

    WIP: accessory detail screen

---
+++ src/screens/accessorydetail/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import type { AccessoryDetailProps } from '~/typings';
+
+const AccessoryDetail = ({ route }: AccessoryDetailProps): JSX.Element => {
+  return <View></View>;
+};
+
+export default AccessoryDetail;
