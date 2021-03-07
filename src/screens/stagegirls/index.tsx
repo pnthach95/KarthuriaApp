c031cb31739a546d20c12e3657e5b35a3dcf3d12
Pham Ngoc Thach <thachxyz123@gmail.com>
2021-03-07 12:54:56 +0700

    Filter effect

--- src/screens/stagegirls/index.tsx
+++ src/screens/stagegirls/index.tsx
@@ -120,9 +120,12 @@ const StageGirls = ({ navigation }: StageGirlsScreenProps): JSX.Element => {
   const insets = useSafeAreaInsets();
   const { colors } = useTheme();
   const bottomSheetModalRef = useRef<BottomSheetModal>(null);
-  const snapPoints = useMemo(() => ['35%'], []);
+  const snapPoints = useMemo(() => ['40%'], []);
   const [loading, setLoading] = useState(true);
+  /** List to filter */
   const [sgList, setSGList] = useState<TDressBasicInfo[]>([]);
+  /** List to render */
+  const [rsgList, setRSGList] = useState<TDressBasicInfo[]>([]);
   const [filter, setFilter] = useState<TFilter>({
     characters: charaImgs.map(() => true),
     elements: [true, true, true, true, true, true, true],
@@ -161,6 +164,96 @@ const StageGirls = ({ navigation }: StageGirlsScreenProps): JSX.Element => {
     };
     void loadData();
   }, []);
+
+  useEffect(() => {
+    if (sgList.length > 0) {
+      const afterFilter = sgList.filter((item) => {
+        const p = () => {
+          switch (item.base.roleIndex.role) {
+            case 'front':
+              return 0;
+            case 'middle':
+              return 1;
+            default:
+              return 2;
+          }
+        };
+        const characterToIndex = () => {
+          switch (item.basicInfo.character) {
+            case 101:
+              return 0;
+            case 102:
+              return 1;
+            case 103:
+              return 2;
+            case 104:
+              return 3;
+            case 105:
+              return 4;
+            case 106:
+              return 5;
+            case 107:
+              return 6;
+            case 108:
+              return 7;
+            case 109:
+              return 8;
+            case 201:
+              return 9;
+            case 202:
+              return 10;
+            case 203:
+              return 11;
+            case 204:
+              return 12;
+            case 205:
+              return 13;
+            case 301:
+              return 14;
+            case 302:
+              return 15;
+            case 303:
+              return 16;
+            case 304:
+              return 17;
+            case 305:
+              return 18;
+            case 401:
+              return 19;
+            case 402:
+              return 20;
+            case 403:
+              return 21;
+            case 404:
+              return 22;
+            case 405:
+              return 23;
+            case 501:
+              return 24;
+            case 502:
+              return 25;
+            case 503:
+              return 26;
+            default:
+              return -1;
+          }
+        };
+        const checkRarity = filter.rarity[item.basicInfo.rarity - 2];
+        const checkAtkType = filter.attackType[item.base.attackType - 1];
+        const checkPosition = filter.position[p()];
+        const checkElement = filter.elements[item.base.attribute - 1];
+        const checkCharacter = filter.characters[characterToIndex()];
+        return (
+          checkRarity &&
+          checkAtkType &&
+          checkPosition &&
+          checkElement &&
+          checkCharacter
+        );
+      });
+      setRSGList(afterFilter);
+    }
+  }, [filter, sgList]);
 
   const openSheet = () => bottomSheetModalRef.current?.present();
 
@@ -214,6 +307,14 @@ const StageGirls = ({ navigation }: StageGirlsScreenProps): JSX.Element => {
     );
   };
 
+  const emptyList = () => {
+    return (
+      <View style={[AppStyles.flex1, AppStyles.center]}>
+        <Text>No data. Please check filter.</Text>
+      </View>
+    );
+  };
+
   if (loading) {
     return <Kirin />;
   }
@@ -222,10 +323,11 @@ const StageGirls = ({ navigation }: StageGirlsScreenProps): JSX.Element => {
     return (
       <>
         <FlatList
-          data={sgList}
+          data={rsgList}
           keyExtractor={keyExtractor}
           renderItem={renderItem}
           style={top}
+          ListEmptyComponent={emptyList}
           numColumns={2}
           initialNumToRender={12}
         />
