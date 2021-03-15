097bd4db33fdcb35cbfb8c026157cea2f0744202
Pham Ngoc Thach <thachxyz123@gmail.com>
2021-03-15 11:34:01 +0700

    Add border radius

--- src/components/sheet/background/index.tsx
+++ src/components/sheet/background/index.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
+import { StyleSheet } from 'react-native';
 import { useTheme } from 'react-native-paper';
 import { BottomSheetBackgroundProps } from '@gorhom/bottom-sheet';
 import { View } from 'react-native';
+
+const styles = StyleSheet.create({
+  radius: {
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+  },
+});
 
 const CustomBackground = ({
   style,
 }: BottomSheetBackgroundProps): JSX.Element => {
   const { colors } = useTheme();
 
-  return <View style={[style, { backgroundColor: colors.card }]} />;
+  return (
+    <View style={[style, styles.radius, { backgroundColor: colors.card }]} />
+  );
 };
 
 export default CustomBackground;
