ac7217836d8ac5551be6216d2bea3b5c4f17a6d9
Pham Ngoc Thach <thachxyz123@gmail.com>
2023-02-28 15:46:34 +0700

    linking

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -28,9 +28,33 @@ import StageGirlDetailScreen from 'screens/stagegirldetail';
 import useStore, {onSwitchMainRoute, useHydration} from 'store';
 import {Dark, Light} from 'theme';
 import Tabs from './tabs';
+import type {LinkingOptions} from '@react-navigation/native';
 import type {RootStackParamList} from 'typings/navigation';
 
 const Stack = createStackNavigator<RootStackParamList>();
+
+const linking: LinkingOptions<RootStackParamList> = {
+  prefixes: ['https://karth.top'],
+  config: {
+    initialRouteName: 'Main',
+    screens: {
+      Main: {
+        screens: {
+          StageGirlsScreen: 'dress',
+          MemoirsScreen: 'equip',
+        },
+      },
+      Characters: 'chara',
+      CharacterDetail: 'chara/:id',
+      StageGirlDetail: 'dress/:id',
+      MemoirDetail: 'equip/:id',
+      Accessories: 'accessory',
+      AccessoryDetail: 'accessory/:id',
+      Enemies: 'enemy',
+      EnemyDetail: 'enemy/:id',
+    },
+  },
+};
 
 dayjs.extend(customParseFormat);
 dayjs.extend(duration);
@@ -39,7 +63,6 @@ dayjs.extend(localizedFormat);
 const Routes = () => {
   const hydrated = useHydration();
   const {i18n} = useTranslation();
-  const mainRoute = useStore(s => s.mainRoute);
   const options = useStore(s => s.options);
   const language = useStore(s => s.language);
   const theme = options.isDark ? Dark : Light;
@@ -70,51 +93,48 @@ const Routes = () => {
         <PaperProvider theme={theme}>
           <ErrorBoundary FallbackComponent={CustomFallback}>
             <BottomSheetModalProvider>
-              <NavigationContainer theme={theme}>
+              <NavigationContainer
+                fallback={<Kirin />}
+                linking={linking}
+                theme={theme}>
                 <Stack.Navigator
                   screenOptions={{headerBackTitle: 'Back', headerShown: false}}>
-                  {mainRoute === 'SPLASH' ? (
-                    <Stack.Screen component={Kirin} name="Splash" />
-                  ) : (
-                    <>
-                      <Stack.Screen component={Tabs} name="Main" />
-                      <Stack.Screen
-                        component={CharactersScreen}
-                        name="Characters"
-                        options={{headerShown: true}}
-                      />
-                      <Stack.Screen
-                        component={CharacterDetailScreen}
-                        name="CharacterDetail"
-                      />
-                      <Stack.Screen
-                        component={StageGirlDetailScreen}
-                        name="StageGirlDetail"
-                      />
-                      <Stack.Screen
-                        component={MemoirDetailScreen}
-                        name="MemoirDetail"
-                      />
-                      <Stack.Screen
-                        component={AccessoriesScreen}
-                        name="Accessories"
-                        options={{headerShown: true}}
-                      />
-                      <Stack.Screen
-                        component={AccessoryDetailScreen}
-                        name="AccessoryDetail"
-                      />
-                      <Stack.Screen
-                        component={EnemiesScreen}
-                        name="Enemies"
-                        options={{headerShown: true}}
-                      />
-                      <Stack.Screen
-                        component={EnemyDetailScreen}
-                        name="EnemyDetail"
-                      />
-                    </>
-                  )}
+                  <Stack.Screen component={Tabs} name="Main" />
+                  <Stack.Screen
+                    component={CharactersScreen}
+                    name="Characters"
+                    options={{headerShown: true}}
+                  />
+                  <Stack.Screen
+                    component={CharacterDetailScreen}
+                    name="CharacterDetail"
+                  />
+                  <Stack.Screen
+                    component={StageGirlDetailScreen}
+                    name="StageGirlDetail"
+                  />
+                  <Stack.Screen
+                    component={MemoirDetailScreen}
+                    name="MemoirDetail"
+                  />
+                  <Stack.Screen
+                    component={AccessoriesScreen}
+                    name="Accessories"
+                    options={{headerShown: true}}
+                  />
+                  <Stack.Screen
+                    component={AccessoryDetailScreen}
+                    name="AccessoryDetail"
+                  />
+                  <Stack.Screen
+                    component={EnemiesScreen}
+                    name="Enemies"
+                    options={{headerShown: true}}
+                  />
+                  <Stack.Screen
+                    component={EnemyDetailScreen}
+                    name="EnemyDetail"
+                  />
                 </Stack.Navigator>
               </NavigationContainer>
               <BlurStatusBar />
